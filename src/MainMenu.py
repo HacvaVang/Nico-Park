2df6bc26e616ccf4a0c1d7861d255cd47022e240
HacvaVang <phuthinh1104@gmail.com>
2026-03-28 06:00:42 +0700

    ...

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -12,91 +12,72 @@ try:
 except Exception as e:
     print(f"Could not load font: {e}")
 
+class SoundManager:
+    _instance = None
+    player = None
+
+    @classmethod
+    def get_player(cls):
+        if cls.player is None:
+            cls.player = media.Player()
+        return cls.player
+
+    @classmethod
+    def play_bgm(cls, path):
+        try:
+            player = cls.get_player()
+            if player.source: # Don't restart if already playing
+                return
+            
+            music = media.load(path, streaming=True)
+            looper = media.SourceGroup(music.audio_format, None)
+            looper.loop = True
+            looper.queue(music)
+            
+            player.queue(looper)
+            player.play()
+        except Exception as e:
+            print(f"Audio Error: {e}")
+
+# 2. UPDATED MAIN MENU
 class MainMenu(Menu):
     def __init__(self):
         super(MainMenu, self).__init__("Nico Park")
         
-        # You can override the font that cocos uses:
-        self.font_title['font_name'] = 'Pixels'
-        self.font_title['font_size'] = 72
-        self.font_title['color'] = (255, 255, 255, 255)
-        
-        self.font_item['font_name'] = 'Pixels'
-        self.font_item['color'] = (200, 200, 200, 255)
-        self.font_item['font_size'] = 32
-        
-        self.font_item_selected['font_name'] = 'Pixels'
-        self.font_item_selected['color'] = (255, 255, 255, 255)
-        self.font_item_selected['font_size'] = 46
+        # UI Styles
+        self.font_title.update({'font_name': 'Pixels', 'font_size': 72})
+        self.font_item.update({'font_name': 'Pixels', 'font_size': 32})
+        self.font_item_selected.update({'font_name': 'Pixels', 'font_size': 46})
 
-        # Item Definitions
-        items = []
-        items.append(MenuItem('New Game', self.on_new_game))
-        items.append(MenuItem('Options', self.on_options))
-        items.append(MenuItem('About', self.on_about))
-        items.append(MenuItem('Exit', self.on_exit))
+        items = [
+            MenuItem('New Game', self.on_new_game),
+            MenuItem('Options', self.on_options),
+            MenuItem('About', self.on_about),
+            MenuItem('Exit', self.on_exit)
+        ]
 
-        # Add items to the menu
         self.create_menu(items, zoom_in(), zoom_out())
-        self.menu_valign = CENTER
-        self.menu_halign = CENTER
-
-        try:
-            bg_music = media.load('assets/sound/Bounds.wav', streaming=True)            
-            looper = media.SourceGroup(bg_music.audio_format, None)
-            looper.loop = True
-            looper.queue(bg_music)
-
-            self.bg_player = media.Player()
-            self.bg_player.queue(looper)
-            self.bg_player.play()
-
-
-        except Exception as e:
-            print(f"Could not load background music: {e}")
+        
+        # Start music safely via SoundManager
+        SoundManager.play_bgm('assets/sound/Bounds.wav')
 
     def on_new_game(self):
-        # Start the game scene
-        print("New Game clicked")
-        if hasattr(self, 'bg_player'):
-            self.bg_player.pause()
-            self.bg_player.delete()        
-        game_scene = create_game_scene()
-        director.replace(game_scene)
-        
+        # Stop music before heavy scene loading
+        if SoundManager.player:
+            SoundManager.player.pause() 
+        director.push(create_game_scene())
+
     def on_options(self):
-        print("Options clicked")
-        options_scene = Scene()
-        options_scene.add(cocos.layer.ColorLayer(20, 40, 60, 255), z=0)
-        options_scene.add(OptionsMenu(), z=1)
-        director.push(options_scene)
+        # Use push with a tiny delay to let OpenGL breathe
+        clock.schedule_once(lambda dt: director.push(Scene(OptionsMenu())), 0.01)
 
     def on_about(self):
-        print("About clicked")
-        about_scene = Scene()
-        about_scene.add(cocos.layer.ColorLayer(20, 40, 60, 255), z=0)
-        about_scene.add(AboutMenu(), z=1)
-        director.push(about_scene)
+        clock.schedule_once(lambda dt: director.push(Scene(AboutMenu())), 0.01)
 
     def on_exit(self):
-        # Exit the application properly
-        director.window.close()
-        sys.exit(0)
+        app.exit()
 
-    def on_menu_key_press(self, symbol, modifiers):
-        # Phát tiếng 'ting' nhẹ khi đổi item
-        # pygame.mixer.Sound("assets/sfx/select.wav").play()
-        super(MainMenu, self).on_menu_key_press(symbol, modifiers)
 
-def create_main_menu():
-    scene = Scene()
-    # Adding a simple colored background
-    background = cocos.layer.ColorLayer(20, 40, 60, 255)
-    scene.add(background, z=0)
-    
-    menu = MainMenu()
-    scene.add(menu, z=1)
-    return scene
 
 class OptionsMenu(Menu):
     def __init__(self):
@@ -119,7 +100,7 @@ class OptionsMenu(Menu):
         items.append(MenuItem('Fullscreen: Off', self.on_dummy))
         items.append(MenuItem('Back', self.on_back))
 
-        self.create_menu(items, shake(), shake_back())
+        self.create_menu(items, zoom_in(), zoom_out())
 
     def on_dummy(self):
         pass
@@ -145,14 +126,24 @@ class AboutMenu(Menu):
         self.font_item_selected['font_size'] = 46
 
         items = []
-        items.append(MenuItem('Created by Nico Park Team', self.on_dummy))
+        items.append(MenuItem('Game nhái Pico Park', self.on_dummy))
         items.append(MenuItem('Version 1.0', self.on_dummy))
         items.append(MenuItem('Back', self.on_back))
 
-        self.create_menu(items, shake(), shake_back())
+        self.create_menu(items, zoom_in(), zoom_out())
 
     def on_dummy(self):
         pass
 
     def on_back(self):
         director.pop()
+
+def create_main_menu():
+    scene = Scene()
+    # Adding a simple colored background
+    background = cocos.layer.ColorLayer(20, 40, 60, 255)
+    scene.add(background, z=0)
+    
+    menu = MainMenu()
+    scene.add(menu, z=1)
+    return scene
